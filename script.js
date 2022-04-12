1c9fddfdad97dec6044498c85db42470dfea509e
Dio Lemos <pedrodiogenes@outlook.com>
2022-04-12 19:07:42 -0300

    Update script.js
    too much delay when the event listener is first executed

--- script.js
+++ script.js
@@ -9,9 +9,23 @@ btns.forEach(btn=>{
         
         if(e.target.id == 'prev'){
             if(currentImageIndex==1){return}
-            
+            //Selects the current active img and removes active
+            document.querySelector(`li[data-image-index="${currentImageIndex}"]`).classList.toggle('active');
+            //decrements counter by one
+            currentImageIndex --  
+            //selects the one image under the previous and toggles
+            //the active class
+            document.querySelector(`li[data-image-index="${currentImageIndex}"]`).classList.toggle('active');
+            return
         }else{
-
+            if(currentImageIndex == images.length){return}
+            //Selects the current active img and removes active
+            document.querySelector(`li[data-image-index="${currentImageIndex}"]`).classList.toggle('active');
+            //increments counter by one
+            currentImageIndex ++  
+            //selects the one image above the previous and toggles
+            //the active class
+            document.querySelector(`li[data-image-index="${currentImageIndex}"]`).classList.toggle('active');
         }
     })
 })
